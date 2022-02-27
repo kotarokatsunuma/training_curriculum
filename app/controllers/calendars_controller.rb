f78f51fa62727fdeb70d72d1369ecf680ecd2bc4
kotarokatsunuma <katsunuma1113@gmail.com>
2022-02-27 15:35:58 +0900

    Revert "Merge pull request #8 from kotarokatsunuma/second_training"
    This reverts commit ba5b5afc9c269398dbe3e475b7e9e166d75047dd, reversing
    changes made to 5f007148a80f9afa30724555abc85365dd22754e.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -35,13 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = Date.today.wday[x+1]
-      if wday_num >= 7
-        wday_num = wday_num -7
-      end
-
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans,:wday =>today_plans}
-
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
